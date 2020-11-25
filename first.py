51494585dcd59f2cdc2694ecbde75e0f6b9ed151
Zabelin VO <zabelin@yandexlyceum.ru>
2020-11-25 11:16:19 +0900

    1

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print('My first git-repo')
+n = int(input())
+soldiers = []
+for _ in range(n):
+    soldiers.append(input())
+k = int(input())
+m = int(input())
+for _ in range(m):
+    del soldiers[k - 1::k]
+for soldier in soldiers:
+    print(soldier)
